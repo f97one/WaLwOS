e3b591024e46e9418692338607b5395ee615a57c
HAJIME Fukuna <f97one@gmail.com>
2021-02-20 21:56:05 +0900

    InputMethodのServiceをとりあえず作成。

---
+++ app/src/main/java/net/formula97/androidapp/walwos/StrokeInputService.kt
@@ -0,0 +1,13 @@
+package net.formula97.androidapp.walwos
+
+import android.inputmethodservice.InputMethodService
+import android.view.View
+
+class StrokeInputService() : InputMethodService() {
+
+    override fun onCreateInputView(): View {
+        // todo キーボードを作って返す処理を書く
+
+        return super.onCreateInputView()
+    }
+}
