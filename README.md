0cc44779158241be94fea6cb86ac06b206003f82
HAJIME Fukuna <f97one@gmail.com>
2021-02-20 17:43:41 +0900

    ひとまず作成直後を追加。

---
+++ README.md
@@ -0,0 +1,5 @@
+# WaLwOS
+
+Write a Letter with One Stroke
+
+WaLwOS (pronounced "wa-ro-su") is an input method editor app for Android, abbreviation of "Write a Letter with One Stroke".
---
+++ app/src/main/java/net/formula97/androidapp/walwos/MainActivity.kt
@@ -0,0 +1,11 @@
+package net.formula97.androidapp.walwos
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
